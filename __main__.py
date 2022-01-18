00398d0fb522c5618b636921862f92036859ffd2
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-01-17 21:23:22 -0500

    Added percentile approximation function

--- __main__.py
+++ __main__.py
@@ -7,12 +7,23 @@ class NumberStatistics:
     def __init__(self, numbers: list):
         self.numbers = sorted(numbers)
         try:
+
             self.mean = self.get_mean()
             self.median = self.get_median()
             self.mode = self.get_mode()
             self.q1 = self.get_median(data=[x for x in self.numbers if x < self.median])
             self.q2 = self.median
             self.q3 = self.get_median(data=[x for x in self.numbers if x > self.median])
+            self.p10 = self.get_percentile(p=10)
+            self.p20 = self.get_percentile(p=20)
+            self.p30 = self.get_percentile(p=30)
+            self.p40 = self.get_percentile(p=40)
+            self.p50 = self.get_percentile(p=50)
+            self.p60 = self.get_percentile(p=60)
+            self.p70 = self.get_percentile(p=70)
+            self.p80 = self.get_percentile(p=80)
+            self.p90 = self.get_percentile(p=90)
+
         except ZeroDivisionError:
             print('Your entry cannot be blank.\n\n')
     
@@ -31,7 +42,10 @@ class NumberStatistics:
         '''
         def calculation(self, **kwargs):
             data = self.find_data(kwargs)
-            tendency = function(data)
+            if 'p' in kwargs:
+                tendency = function(data, kwargs['p'])
+            else:
+                tendency = function(data)
             return tendency
         return calculation
 
@@ -78,6 +92,28 @@ class NumberStatistics:
                 max_occurrences = count[number]
 
         return key
+
+    @calculate_statistics
+    def get_percentile(data, percentile):
+        '''
+        Returns the value at which the percentile% of data is below or equal to..
+        '''
+        rank = (percentile / 100) * (len(data) + 1)
+        if rank % 1 == 0:
+            if percentile == 100:
+                return data[:-1]
+            else:
+                return data[int(rank) - 1]
+        else:
+
+            lower_datum = floor(rank) - 1
+            if lower_datum < 0: lower_datum = 0
+            
+            upper_datum = ceil(rank) - 1
+            if upper_datum >= len(data): upper_datum = len(data) - 1
+
+            return (data[lower_datum] + data[upper_datum]) / 2
+
 
 if __name__ == '__main__':
     while True:
